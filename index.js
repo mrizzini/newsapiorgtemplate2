e14579a8fad0514b5fc2d445349fa552928b4e27
Matthew Rizzini <matthewrizzini@gmail.com>
2017-11-29 00:16:31 +0000

    added document.getElementByID to list.innerHTML

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ $(document).ready(function() {
 	//   });
 	    $('#source').submit(function(event) {
 		      event.preventDefault();
-		      list.innerHTML = "";
+		      document.getElementById('list').innerHTML = "";
 		 // alert(document.getElementById("selection").value);
 		 // alert(document.getElementById("selection").name);
 		      $.ajax({
